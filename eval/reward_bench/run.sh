abeeae42402a82f5d246ca1a1b3a6867bee95920
schrieffer-z <schrieffer.zsy@gmail.com>
2025-08-02 14:40:14 +0800

    init

--- eval/reward_bench/run.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=5 python eval_rewardbench_sarm_llama.py \
-    --batch_size=1 \
-    --model /NAS/zhangsy/models/Skywork/Skywork-Reward-Llama-3.1-8B-v0.2 \
-    --tokenizer /NAS/zhangsy/models/Skywork/Skywork-Reward-Llama-3.1-8B-v0.2 \
-    --dataset /NAS/zhangsy/datasets/allenai/reward-bench-2 \
