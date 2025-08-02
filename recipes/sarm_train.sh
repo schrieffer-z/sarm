fe70132ad6df84ce6988e72820d29285355ce675
schrieffer-z <schrieffer.zsy@gmail.com>
2025-08-02 14:42:00 +0800

    init

--- recipes/sarm_train.sh
+++ recipes/sarm_train.sh
@@ -11,26 +11,19 @@ num_train_epochs=3
 
 # base model path & output path
 sarm_base_model=llama
-# 模型的本地存储路径
 model_name=
-# 需要按照这样的格式，sae的path和sarm的path是一样的
 sae_title=Llama-3.1-8B-Instruct_sequence_Latent65536_Layer16_K192_1B
 sae_path=.../$sae_title.pt
 output_path=.../$sae_title-SARM-woTopK-LastToken-1
-# 这里直接写parquet文件的位置
 train_set_path=
 
 # sarm config
 sae_use_sequence_level=false
 sarm_use_baseline=false
 sarm_use_topk=false
-# sarm作为rm在训练的过程中，sae的参数是否变化：
-# sarm_train_mode=3: 表示sae参与训练，且受到preference loss以及reconstruction loss的影响
-# sarm_train_mode=1: 表示sae不参与参与训练
+# sarm_train_mode=3: sae will be updated by preference loss and reconstruction loss
+# sarm_train_mode=1: sae won't not be updated
 sarm_train_mode=3
-# sarm_train_mode=3时有效, 辅助loss需要的系数
-# sarm作为rm在训练的过程中，sae受到preference loss以及reconstruction loss的影响
-# loss = preference_loss + sarm_rec_lambda * reconstruction_loss
 sarm_rec_lambda=1
 
 
