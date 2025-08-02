f642e8d8fa185465cf8a62b3cd353bb28af3c962
schrieffer-z <schrieffer.zsy@gmail.com>
2025-08-02 14:52:23 +0800

    init

--- eval/rm_bench/run.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 python eval_rmbench_sarm_llama.py \
-    --model /NAS/zhangsy/models/Skywork/Skywork-Reward-Llama-3.1-8B-v0.2/ \
-    --datapath data/total_dataset.json \
-    --batch_size 1 \
-    --trust_remote_code \
-    --chat_template tulu
